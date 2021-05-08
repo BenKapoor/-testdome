f5e6023309bff2a05ff2e1de84a535c40f216991
BenKapoor <benjaminkapoor@yahoo.fr>
2021-05-08 13:43:49 +0200

    Create 5. Sessions.sql

---
+++ 5. Sessions.sql
@@ -0,0 +1,23 @@
+/**
+App usage data are kept in the following table:
+**/
+TABLE sessions
+  id INTEGER PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  duration DECIMAL NOT NULL
+/**
+Write a query that selects userId and average session duration for each user who has more than one session.
+**/
+
+SELECT userId, AVG(duration) 
+FROM sessions
+GROUP BY userId 
+HAVING COUNT(userId) > 1
+
+/**
+Output
+userId    AVG(duration)    
+-----------------------
+1         12              
+
+**/
